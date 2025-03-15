903cfcf48fcecea1c11abd72337f1ba260aafcbf
Mrking671 <telegrambot969797@gmail.com>
2025-03-15 22:08:08 +0530

    Update Movie.js

--- models/Movie.js
+++ models/Movie.js
@@ -1,12 +1,42 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  file_id: String,
-  file_name: String,
-  file_size: Number,
-  caption: String,
-  tmdbData: Object,
-  createdAt: { type: Date, default: Date.now }
+  file_id: {
+    type: String,
+    required: true,
+    index: true
+  },
+  file_name: {
+    type: String,
+    required: true,
+    text: true
+  },
+  file_size: {
+    type: Number,
+    required: true
+  },
+  caption: {
+    type: String,
+    text: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, { 
+  collection: 'vjcollection',
+  strict: false 
 });
 
-module.exports = mongoose.model('Movie', movieSchema, 'vjcollection');
+// Text index for search
+movieSchema.index({
+  file_name: 'text',
+  caption: 'text'
+}, {
+  weights: {
+    file_name: 5,
+    caption: 1
+  }
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
